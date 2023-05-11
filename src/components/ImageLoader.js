931cd675d28657d2099d9c532c96b474173bd30b
YoloMao <a83506677@126.com>
2023-05-11 16:12:24 +0800

    perf: ImageLoader code style

--- src/components/ImageLoader.js
+++ src/components/ImageLoader.js
@@ -2,25 +2,26 @@ import React from "react";
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function ImageLoader({path, width, height}) {
-    let pre = path;
-    let suf = "";
-    if (pre.lastIndexOf(".") > -1) {
-        pre = pre.substring(0, pre.lastIndexOf("."));
-        suf = path.substring(path.lastIndexOf("."));
+    let prefix = path;
+    let suffix = '';
+    if (path.lastIndexOf('.') > -1) {
+        prefix = path.substring(0, path.lastIndexOf('.'));
+        suffix = path.substring(path.lastIndexOf('.'));
     }
-    let avif = useBaseUrl(pre + ".avif");
-    let webp = useBaseUrl(pre + ".webp");
+    let avif = useBaseUrl(prefix + '.avif');
+    let webp = useBaseUrl(prefix + '.webp');
 
-    let src = "";
-    if (suf == ".jpg" || suf == ".jpeg" || suf == ".png" || suf == ".gif") {
+    let src = '';
+    if (suffix == '.jpg' || suffix == '.jpeg' || suffix == '.png' || suffix == '.gif') {
         src = useBaseUrl(path);
-    } else if (suf == "") {
-        src = useBaseUrl(path + ".png");
+    } else if (suffix == '') {
+        src = useBaseUrl(path + '.png'); // default png
     }
+
     return (
         <picture>
-            <source srcset={avif} type="image/avif" />
-            <source srcset={webp} type="image/webp" />
+            <source srcset={avif} type='image/avif' />
+            <source srcset={webp} type='image/webp' />
             <img src={src} width={width} height={height}/>
         </picture>
     );
